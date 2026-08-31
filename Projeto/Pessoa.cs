3d94b6e90198d11aebb08c3839e4ea0c88ff5c87
Evandro <maarcioherobrine25@gmail.com>
2026-08-31 20:05:08 -0300

    feat: Codificando classe abstrata pessoa

---
+++ Projeto/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿namespace Projeto
+{
+    public static class Pessoa
+    {
+        private string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }       
+
+        public virtual void ExibirInfo()
+        {
+            Console.WriteLine($"Nome:{Nome}");
+        }
+
+    }
+}
