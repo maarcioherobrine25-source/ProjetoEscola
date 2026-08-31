ad6e1c885f95f402ba3df0e61f485dbc53f92196
Evandro <maarcioherobrine25@gmail.com>
2026-08-31 20:20:05 -0300

    feat: codificando a classe professor

--- Projeto/Professor.cs
+++ Projeto/Professor.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projeto
+﻿namespace Projeto
 {
-    internal class Professor
+    class Professor : Pessoa
     {
+        public string Disciplina { get; set; }
+
+        public Professor (String nome, string disciplina): base(nome)
+        {
+            Disciplina = disciplina;
+        }
+        public override void ExibirInfo()
+        {
+            Console.WriteLine($"[Professor]: Nome {Nome}, Disciplina: {Disciplina}");
+        }
     }
 }
